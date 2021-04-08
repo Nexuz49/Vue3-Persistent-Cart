09121edc0572b81a991e918e98fef13e0ed25f92
moalinden <moa.linden1@gmail.com>
2021-04-08 21:07:53 +0200

    varor

--- Vue3 Cart/vuex-cart-backend/server.js
+++ Vue3 Cart/vuex-cart-backend/server.js
@@ -39,7 +39,7 @@ app.post('/cart', (req, res) => {
         }
       });
       if (!cartProductExists) cartProducts.push(newCartProduct);
-      fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 4), () => {
+      fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 9), () => {
         res.setHeader('Cache-Control', 'no-cache');
         res.json(cartProducts);
       });
@@ -70,7 +70,7 @@ app.post('/cart', (req, res) => {
           cartProducts.splice(cartIndexToRemove, 1);
         }
       });
-      fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 4), () => {
+      fs.writeFile(CART_DATA_FILE, JSON.stringify(cartProducts, null, 9), () => {
         res.setHeader('Cache-Control', 'no-cache');
         res.json(cartProducts);
       });
@@ -81,7 +81,7 @@ app.post('/cart', (req, res) => {
   app.delete('/cart/delete/all', (req, res) => {
     fs.readFile(CART_DATA_FILE, () => {
       let emptyCart = [];
-      fs.writeFile(CART_DATA_FILE, JSON.stringify(emptyCart, null, 4), () => {
+      fs.writeFile(CART_DATA_FILE, JSON.stringify(emptyCart, null, 9), () => {
         res.json(emptyCart);
       });
     });
